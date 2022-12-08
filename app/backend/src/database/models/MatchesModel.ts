f58538333466150f6d5db019e407380ddb3a1844
Thiago Mota <th.araujomota@gmail.com>
2022-12-08 14:05:18 -0300

    feat: creates Matches Model

---
+++ app/backend/src/database/models/MatchesModel.ts
@@ -0,0 +1,45 @@
+import { Model, INTEGER, BOOLEAN } from 'sequelize';
+import db from '.';
+
+class Matchs extends Model {
+  declare id: number;
+  declare homeTeam: number;
+  declare homeTeamGoals: number;
+  declare awayTeam: number;
+  declare awayTeamsGoals: number;
+  declare inProgress: boolean;
+}
+
+Matchs.init({
+  id: {
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    type: INTEGER,
+  },
+  homeTeam: {
+    allowNull: false,
+    type: INTEGER,
+  },
+  homeTeamGoals: {
+    allowNull: false,
+    type: INTEGER,
+  },
+  awayTeam: {
+    allowNull: false,
+    type: INTEGER,
+  },
+  awayTeamsGoals: {
+    allowNull: false,
+    type: INTEGER,
+  },
+  inProgress: {
+    allowNull: false,
+    type: BOOLEAN,
+  },
+}, {
+  underscored: true,
+  sequelize: db,
+  modelName: 'matches',
+  timestamps: false,
+});
