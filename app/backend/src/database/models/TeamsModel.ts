349c1866be916807726823b0e9e2ce1e91494074
Thiago Mota <th.araujomota@gmail.com>
2022-12-08 14:04:18 -0300

    feat: creates teams model

---
+++ app/backend/src/database/models/TeamsModel.ts
@@ -0,0 +1,25 @@
+import { Model, INTEGER, STRING } from 'sequelize';
+import db from '.';
+
+class Teams extends Model {
+  declare id: number;
+  declare teamName: string;
+}
+
+Teams.init({
+  id: {
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    type: INTEGER,
+  },
+  teamName: {
+    allowNull: false,
+    type: STRING,
+  },
+}, {
+  underscored: true,
+  sequelize: db,
+  modelName: 'teams',
+  timestamps: false,
+});
