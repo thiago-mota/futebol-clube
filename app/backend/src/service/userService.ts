1067d2cba224370a32aadecacc49554ec3672db5
Thiago Mota <th.araujomota@gmail.com>
2023-01-11 19:56:06 -0300

    feat: adds userToken function

--- app/backend/src/service/userService.ts
+++ app/backend/src/service/userService.ts
@@ -1,15 +1,20 @@
-// import bcryptjs = require('bcryptjs');
 import * as bcryptjs from 'bcryptjs';
+import token from '../helpers/createToken';
 import Users from '../database/models/UsersModel';
 
-const validateUser = async (email: string, password: string) => {
+const userLogin = async (email: string, password: string) => {
   const findUser = await Users.findOne({ where: { email } });
   if (!findUser) {
-    return { status: 400, message: 'Deu ruim' };
+    return { status: 401, message: 'Incorrect email or password' };
   }
 
   const checkPass = await bcryptjs.compare(password, findUser.password);
-  if (!checkPass) return 'Deu pior';
+  if (!checkPass) return { status: 401, message: 'CHECKPASS ---> Incorrect email or password' };
+
+  const userToken = token.createToken(email);
+  return userToken;
+
+  return 'deu bom';
 };
 
-export default { validateUser };
+export default { userLogin };
