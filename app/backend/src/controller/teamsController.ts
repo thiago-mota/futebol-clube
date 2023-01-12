c0a460bf00743b9d886a0feeebee1e9699f81373
Thiago Mota <th.araujomota@gmail.com>
2023-01-12 19:11:01 -0300

    feat: adds findTeamByPk function on controller
    refac: fix identations

--- app/backend/src/controller/teamsController.ts
+++ app/backend/src/controller/teamsController.ts
@@ -3,9 +3,18 @@ import teamsService from '../service/teamsService';
 
 const getAllTeams = async (_request: Request, response: Response) => {
   const allTeams = await teamsService.findAllTeams();
-  return response.status(200).json(allTeams);
+  return response
+    .status(200)
+    .json(allTeams);
 };
 
-const xablau = async () => 'xablau';
+const findTeamByPk = async (request: Request, response: Response) => {
+  const { id } = request.params;
+  const findTeamById = await teamsService.getById(id);
 
-export { getAllTeams, xablau };
+  return response
+    .status(200)
+    .json(findTeamById);
+};
+
+export { getAllTeams, findTeamByPk };
