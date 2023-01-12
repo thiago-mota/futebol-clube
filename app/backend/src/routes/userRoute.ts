a7753ad0518e215d7c50bb0b23dc4ba9c899e644
Thiago Mota <th.araujomota@gmail.com>
2023-01-12 15:55:51 -0300

    feat: adds /validate endpoint

--- app/backend/src/routes/userRoute.ts
+++ app/backend/src/routes/userRoute.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express';
-import userLogin from '../controller/userController';
+import { userLogin, getUserRole } from '../controller/userController';
 
 const loginRoute = Router();
 
 loginRoute.post('/', userLogin);
+loginRoute.get('/validate', getUserRole);
 
 export default loginRoute;
