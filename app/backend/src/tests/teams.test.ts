7133bb43daf87c089b5f8909037bc1e029a637d7
Thiago Mota <th.araujomota@gmail.com>
2023-01-12 19:29:11 -0300

    feat: adds tests for teams endpoints

---
+++ app/backend/src/tests/teams.test.ts
@@ -0,0 +1,37 @@
+import App from '../app';
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+// @ts-ignore
+import chaiHttp = require('chai-http');
+
+chai.use(chaiHttp);
+
+const { app } = new App();
+const { expect } = chai;
+
+describe('Testing /teams endpoint', () => {
+  it('Status 200 if querie is successful', async () => {
+    const response = await chai.request(app).get('/teams');
+    // console.log(response);
+    expect(response.status).to.be.equal(200);
+    expect(response.body).to.have.lengthOf(16)
+  });
+
+  it('Returns all registered teams', async () => {
+    const response = await chai.request(app).get('/teams');
+    expect(response.body).to.have.lengthOf(16)
+  });
+});
+
+describe('Testing /teams/:id endpoint', () => {
+  it('Status 200 if querie is successful', async () => {
+    const response = await chai.request(app).get('/teams/:id')
+    expect(response.status).to.be.equal(200);
+  });
+
+  it('Returns id and name from a single team by his id', async () => {
+    const response = await chai.request(app).get('/teams/1');
+    expect(response.body).to.have.property('id');
+    expect(response.body).to.have.property('teamName');
+  });
+});
