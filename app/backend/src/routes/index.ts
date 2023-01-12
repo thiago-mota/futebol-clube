60090f6b1b9774bbc3591580fec926144233e2dc
Thiago Mota <th.araujomota@gmail.com>
2023-01-12 16:38:46 -0300

    feat: adds teamsRoute on routes index

--- app/backend/src/routes/index.ts
+++ app/backend/src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import loginRoute from './userRoute';
+import teamsRoute from './teamsRoute';
 
 const router = Router();
 
 router.use('/login', loginRoute);
+router.use('/teams', teamsRoute);
 
 export default router;
