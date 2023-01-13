1c7305b7dd86afdb1f27f744c913c9cd27ff136e
Thiago Mota <th.araujomota@gmail.com>
2023-01-13 18:57:29 -0300

    feat: implements update function on endpoint route

--- app/backend/src/routes/matchesRoute.ts
+++ app/backend/src/routes/matchesRoute.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
-import { getAllMatches, addMatch } from '../controller/matchesController';
+import { getAllMatches, addMatch, updateMatchProgress } from '../controller/matchesController';
 import verifyToken from '../middlewares/authMiddleware';
 
 const matchesRoute = Router();
 
 matchesRoute.get('/matches', getAllMatches);
 matchesRoute.post('/matches', verifyToken, addMatch);
+matchesRoute.patch('/matches/:id/finish', updateMatchProgress);
 
 export default matchesRoute;
