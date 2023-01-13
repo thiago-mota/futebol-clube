d4da3619da1853c76ff4fb2e374a818e554d544b
Thiago Mota <th.araujomota@gmail.com>
2023-01-13 14:00:14 -0300

    feat: adds teamHome and teamAway infos on queries return

--- app/backend/src/service/matchesService.ts
+++ app/backend/src/service/matchesService.ts
@@ -1,17 +1,30 @@
 import Matchs from '../database/models/MatchesModel';
+import Teams from '../database/models/TeamsModel';
 
 const findAllMatches = async () => {
-  const allMatches = Matchs.findAll();
+  const allMatches = Matchs.findAll({
+    include: [
+      { model: Teams, as: 'teamHome', attributes: ['teamName'] },
+      { model: Teams, as: 'teamAway', attributes: ['teamName'] },
+    ] });
   return allMatches;
 };
 
 const findAllInProgress = async () => {
-  const allInProgress = await Matchs.findAll({ where: { inProgress: true } });
+  const allInProgress = await Matchs.findAll({ where: { inProgress: true },
+    include: [
+      { model: Teams, as: 'teamHome', attributes: ['teamName'] },
+      { model: Teams, as: 'teamAway', attributes: ['teamName'] },
+    ] });
   return allInProgress;
 };
 
 const findAllNotInProgress = async () => {
-  const allInProgress = await Matchs.findAll({ where: { inProgress: false } });
+  const allInProgress = await Matchs.findAll({ where: { inProgress: false },
+    include: [
+      { model: Teams, as: 'teamHome', attributes: ['teamName'] },
+      { model: Teams, as: 'teamAway', attributes: ['teamName'] },
+    ] });
   return allInProgress;
 };
 
