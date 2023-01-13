e6d08a6749b3f9f5bc38798e1d34c89dd865f8ea
Thiago Mota <th.araujomota@gmail.com>
2023-01-13 15:25:52 -0300

    feat: uses token validation middleware on matches post route

--- app/backend/src/routes/matchesRoute.ts
+++ app/backend/src/routes/matchesRoute.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import { getAllMatches, addMatch } from '../controller/matchesController';
+import verifyToken from '../middlewares/authMiddleware';
 
 const matchesRoute = Router();
 
 matchesRoute.get('/matches', getAllMatches);
-matchesRoute.post('/matches', addMatch);
+matchesRoute.post('/matches', verifyToken, addMatch);
 
 export default matchesRoute;
