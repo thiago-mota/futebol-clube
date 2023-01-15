d6b7ff7089ac41296ae7adb3c34dbbe0d35320d2
Thiago Mota <th.araujomota@gmail.com>
2023-01-15 15:03:41 -0300

    feat: implements updateGameResult on endpoint

--- app/backend/src/routes/matchesRoute.ts
+++ app/backend/src/routes/matchesRoute.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express';
-import { getAllMatches, addMatch, updateMatchProgress } from '../controller/matchesController';
+import { getAllMatches, addMatch, updateMatchProgress,
+  updateGameResult } from '../controller/matchesController';
 import verifyToken from '../middlewares/authMiddleware';
 
 const matchesRoute = Router();
 
 matchesRoute.get('/matches', getAllMatches);
 matchesRoute.post('/matches', verifyToken, addMatch);
+matchesRoute.patch('/matches/:id/', updateGameResult);
 matchesRoute.patch('/matches/:id/finish', updateMatchProgress);
 
 export default matchesRoute;
